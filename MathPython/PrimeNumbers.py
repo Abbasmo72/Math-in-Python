d5130be70ee83a45a89f8f2ca5edc19fff60812b
Abbas Mobasseri <110355737+Abbasmo72@users.noreply.github.com>
2024-10-21 14:00:46 +0330

    PrimeNumbers.py

--- MathPython/PrimeNumbers.py
+++ MathPython/PrimeNumbers.py
@@ -1,4 +1,4 @@
-def ispraime(n):
+def isprime(n):
     if n <= 1:
         return False
     for x in range(2, n):
@@ -8,7 +8,7 @@ def ispraime(n):
         return True
     
 n = int(input('Enter The Number: '))
-if ispraime(n):
+if isprime(n):
     print(f'{n} is prime')
 else:
     print(f'{n} not prime')
