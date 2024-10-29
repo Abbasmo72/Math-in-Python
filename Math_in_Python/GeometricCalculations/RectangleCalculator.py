6c6905969608837661af44b74ccd96870eb7a40e
Abbas Mobasseri <110355737+Abbasmo72@users.noreply.github.com>
2024-10-29 09:33:31 +0330

    Add files via upload

---
+++ Math_in_Python/GeometricCalculations/RectangleCalculator.py
@@ -0,0 +1,11 @@
+# Get length and width from the user
+length = float(input("Enter the rectangle's length: "))
+width = float(input("Enter the rectangle's width: "))
+
+# Calculate area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+
+# Display results
+print("The area of the rectangle is:", area)
+print("The perimeter of the rectangle is:", perimeter)
